e905ebceb32516eb82f4e36f50bce31012601b99
Amber Kimbrough <akimbrough14@gmail.com>
2023-04-09 16:27:25 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+/*
+function filter(array, cb){
+    let newArray = []
+    for(const driver of array){
+        if(cb(driver)){
+            newArray.push(driver)
+        }
+    }
+    return newArray
+}
+*/
+
+function findMatching(array, string){
+    let newArray = []
+    for(const driver of array){
+        if(driver.toUpperCase() === string.toUpperCase()){
+            newArray.push(driver)
+            
+        }
+    }
+    return newArray
+}
+
+function fuzzyMatch(array, string){
+    let newArray = []
+    for(const driver of array){
+        if(driver.startsWith(string)){
+            newArray.push(driver)
+        }
+    }
+    return newArray
+}
+
+function matchName(array, string){
+    let newArray = []
+    for(const driver of array){
+        if(driver.name === string){
+            newArray.push(driver)
+        }
+
+    }
+    return newArray
+}
+
+
+
+
+//console.log(findMatching(drivers, "Bobby"))
+console.log(fuzzyMatch(drivers, "Sa"))
+
+
+
